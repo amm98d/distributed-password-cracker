98a9efc1e02dc9761a42918dbf4473fccc2f4da9
Ahmed Mustafa Malik <ahmedmustafamalik98@gmail.com>
2021-06-13 13:24:31 +0500

    [New Development] All slaves receive initial instructions from master

--- main.c
+++ main.c
@@ -8,7 +8,8 @@
 #include <unistd.h>
 #include <math.h>
 
-int main(int argc, char* argv[]){	
+int main(int argc, char *argv[])
+{
 
 	//char id[] = "1234";
 	//char salt[] = "$6$lKnEdTX3$";
@@ -21,22 +22,24 @@ int main(int argc, char* argv[]){
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-/*	Regarding MPI Send & Receive:				*/
-/*	===========================================	*/
-/*	array size communication on tag 		= 0 */
-/*	array itself communication on tag 		= 1 */
-/*	number to search communication on tag 	= 2 */
-/*	abort message communication on tag 		= 3 */
+	/*	Regarding MPI Send & Receive:				*/
+	/*	===========================================	*/
+	/*	array size communication on tag 		= 0 */
+	/*	array itself communication on tag 		= 1 */
+	/*	number to search communication on tag 	= 2 */
+	/*	abort message communication on tag 		= 3 */
 
 	// master
-	if (rank==0){
+	if (rank == 0)
+	{
 		// clearing the console
 		int sysRet = system("clear");
-		if (sysRet==-1){
+		if (sysRet == -1)
+		{
 			printf("System Method Failed.\n");
 			return 0;
 		}
-		
+
 		printf("PDC Project - Distributed Password Cracker\n");
 		printf("============================================\n\n");
 
@@ -61,26 +64,31 @@ int main(int argc, char* argv[]){
 		// =============================================================================================
 
 		//Reading file and extracting required line
-		FILE * fp;
-		char * line = NULL;
+		FILE *fp;
+		char *line = NULL;
 		size_t len = 0;
 		ssize_t read;
-		
+
 		// Reading /etc/shadow
 		// For now i have copied the content in a local file sample.txt
 		// We can replace it with /etc/shadow path and run with sudo command to give the permission to read /etc/shadow file in the program
 		fp = fopen("/etc/shadow", "r");
-		if (fp == NULL){
+		if (fp == NULL)
+		{
 			printf("File not found\n");
 			fclose(fp);
 			MPI_Finalize();
 			return 0;
 		}
 
-		while ((read = getline(&line, &len, fp)) != -1) {
+		while ((read = getline(&line, &len, fp)) != -1)
+		{
 			//comparing username
 			int result = strncmp(username, line, nameLen);
-			if(result == 0){break;}
+			if (result == 0)
+			{
+				break;
+			}
 		}
 		fclose(fp);
 
@@ -88,25 +96,27 @@ int main(int argc, char* argv[]){
 		char *targetHash = malloc(98 * sizeof(char));
 		memcpy(targetHash, line + nameLen + 1, 98 * sizeof(char));
 		int targetHashLen = (int)strlen(targetHash);
-	
+
 		// =============================================================================================
 		// STEP-3: Distribute workloads
 		// =============================================================================================
 
 		// [TODO]
-/*		1. Calculate total number of permutations*/
-		int numDigits = 4, totalPermutations = pow(26, numDigits);
-		printf("%d\n\n",totalPermutations);
-		int iproc, chunkStart=0, chunkLength=totalPermutations/(nprocs-1), abort=0;
+		/*		1. Calculate total number of permutations*/
+		int numDigits = 2, totalPermutations = pow(26, numDigits);
+		printf("%d\n\n", totalPermutations);
+		int iproc, chunkStart = 0, chunkLength = totalPermutations / (nprocs - 1), abort = 0;
 		MPI_Status status;
 
-		printf("Master: The hash to crack is: %s\n\n",targetHash);
+		printf("Master: The hash to crack is: %s\n\n", targetHash);
 
 		// each process gets equal range i.e., (total permutations) / (#slaves)
-		for (iproc=1;iproc<nprocs;iproc++){
-			MPI_Send(&targetHashLen, 1, MPI_INT, iproc, 0, MPI_COMM_WORLD);// targetHashLen sent with tag 0
-			MPI_Send(targetHash, targetHashLen, MPI_INT, iproc, 1, MPI_COMM_WORLD);// targetHash sent with tag 1
-			// MPI_Send(targetHash, hashLen, MPI_CHAR, iproc, 1, MPI_COMM_WORLD);// number to search sent with tag 1
+		for (iproc = 1; iproc < nprocs; iproc++)
+		{
+			MPI_Send(&targetHashLen, 1, MPI_INT, iproc, 0, MPI_COMM_WORLD);			// targetHashLen sent with tag 0
+			MPI_Send(targetHash, targetHashLen, MPI_INT, iproc, 1, MPI_COMM_WORLD); // targetHash sent with tag 1
+			MPI_Send(&chunkStart, 1, MPI_INT, iproc, 2, MPI_COMM_WORLD);			// chunkStart sent with tag 2
+			MPI_Send(&chunkLength, 1, MPI_INT, iproc, 3, MPI_COMM_WORLD);			// chunkLength sent with tag 3
 			chunkStart += chunkLength;
 		}
 
@@ -114,36 +124,42 @@ int main(int argc, char* argv[]){
 		// STEP-4: Sit tight and wait for good news
 		// =============================================================================================
 
-/*		MPI_Recv(&abort, 1, MPI_INT, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, &status);// abort message received with tag 3*/
-/*		printf("Master: Process %d has found the number!\n", status.MPI_SOURCE);*/
-/*		printf("Master: Informing all processes to abort!\n");*/
-/*		// sending abort message*/
-/*		for (iproc=1;iproc<nprocs;iproc++)*/
-/*			MPI_Send(&abort, 1, MPI_INT, iproc, 3, MPI_COMM_WORLD);// abort messages sent with tag 3*/
+		/*		MPI_Recv(&abort, 1, MPI_INT, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, &status);// abort message received with tag 3*/
+		/*		printf("Master: Process %d has found the number!\n", status.MPI_SOURCE);*/
+		/*		printf("Master: Informing all processes to abort!\n");*/
+		/*		// sending abort message*/
+		/*		for (iproc=1;iproc<nprocs;iproc++)*/
+		/*			MPI_Send(&abort, 1, MPI_INT, iproc, 3, MPI_COMM_WORLD);// abort messages sent with tag 3*/
 	}
 
 	// slaves
-	else { 
+	else
+	{
 		// initializing variables
-		int chunkLength, abort=0, targetHashLen;
+		int chunkStart, chunkLength, abort = 0, targetHashLen;
 
-		// each slave receiving targetHashLen and allocating memory
-		MPI_Recv(&targetHashLen, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		char *targetHash = malloc(targetHashLen * sizeof(char));
+		// receiving initial instructions from master
+		MPI_Recv(&targetHashLen, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);		   // receive targetHashLen with tag 0
+		char *targetHash = malloc(targetHashLen * sizeof(char));							   // allocate memory according to targetHashLen
+		MPI_Recv(targetHash, targetHashLen, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // receiving targetHash itself with tag 1
+		MPI_Recv(&chunkStart, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);			   // receive chunkStart with tag 2
+		MPI_Recv(&chunkLength, 1, MPI_INT, 0, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);		   // receive chunkLength with tag 3
 
-		// receiving targetHash itself
-		MPI_Recv(targetHash, targetHashLen, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		printf("Process %d has local data: ",rank);
+		printf("Process %d:\n \
+		\tHash to crack: %s\n \
+		\tChunkStart: %d\n \
+		\tChunkLength: %d\n\n",
+			   rank, targetHash, chunkStart, chunkLength);
 
 		// // receiving the number to search
 		// MPI_Recv(&toSearch, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		
+
 		// int i;
 		// for (i=0;i<chunkLength;i++){
 		// 	printf("%d ",arrayToSearch[i]);
 		// }
 		// printf("\n");
-		
+
 		// // using openmp to create 2 threads
 		// #pragma omp parallel num_threads(2)
 		// {
@@ -172,8 +188,8 @@ int main(int argc, char* argv[]){
 		// 		// waiting for abort message from master with tag 3
 		// 		MPI_Recv(&abort, 1, MPI_INT, 0, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		// 	}
-		// }		
+		// }
 	}
-    MPI_Finalize();
-    return 0;
+	MPI_Finalize();
+	return 0;
 }
